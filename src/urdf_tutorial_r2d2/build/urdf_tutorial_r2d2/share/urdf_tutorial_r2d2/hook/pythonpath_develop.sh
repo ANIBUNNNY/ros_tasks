dbdd61e5707def204379f601e30ac6d51cb27a56
ANIBUNNNY <anik.arun06@gmail.com>
2025-02-15 14:45:05 +0530

    first commit

---
+++ src/urdf_tutorial_r2d2/build/urdf_tutorial_r2d2/share/urdf_tutorial_r2d2/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/anibunny/ros_intermediate/src/urdf_tutorial_r2d2/build/urdf_tutorial_r2d2"
